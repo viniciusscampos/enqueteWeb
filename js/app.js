ca5e578ec88646e4542fcc98d81b27eb0babdbef
Vinicius Campos <Vinicius Campos>
2016-02-28 15:41:31 -0300

    formulario de cadastro

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 var app= angular.module('myApp',[]);
 app.controller('formCtrl', function($scope){	
 	$scope.entrar = function(){
+		//realizará o login pelo site
 		$scope.user = angular.copy($scope.master);
 	};	
+	$scope.facebook = function(){
+		//realizará o login pelo facebook e caso seja a primeira vez do usuário pedirá que ele adicione o cpf para completar o cadastro
+	};
+	$scope.registrar = function(){
+		//realizará o cadastro
+	};
 });
