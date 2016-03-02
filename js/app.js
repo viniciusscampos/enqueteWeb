31ccbed5b8927589c86169d4593e463b1680c2e4
Vinicius Campos <Vinicius Campos>
2016-03-02 09:37:32 -0300

    pagina inicial com formularios de login e cadastro.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 var app= angular.module('myApp',[]);
-app.controller('formCtrl', function($scope){	
+app.controller('formCtrl', function($scope,$window){	
 	$scope.entrar = function(){
 		//realizará o login pelo site
 		$scope.user = angular.copy($scope.master);
+		$window.location.href='usuario.html';
 	};	
 	$scope.facebook = function(){
 		//realizará o login pelo facebook e caso seja a primeira vez do usuário pedirá que ele adicione o cpf para completar o cadastro
+		console.log("facebook");
 	};
 	$scope.registrar = function(){
 		//realizará o cadastro
 	};
+});
+app.controller('ExampleController', function($scope){	
+	$scope.resposta = {
+		valor: 'sim'
+	};
+	$scope.enviar = function(){
+		//enviará o formulário de respostas
+		console.log("Enviado com sucesso!");
+	};		
 });
