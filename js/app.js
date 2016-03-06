6b2587d40dba739859eab08174dc904bce3018ba
Vinicius Campos <Vinicius Campos>
2016-03-06 11:42:26 -0300

    Página de usuário e formulário da enquete modificados

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 var app= angular.module('myApp',[]);
-app.controller('formCtrl', function($scope,$window){	
+app.controller('appCtrl', function($scope,$window){	
 	$scope.entrar = function(){
 		//realizará o login pelo site
 		$scope.user = angular.copy($scope.master);
-		$window.location.href='usuario.html';
+		$window.location.href='usuario.html';		
 	};	
 	$scope.facebook = function(){
 		//realizará o login pelo facebook e caso seja a primeira vez do usuário pedirá que ele adicione o cpf para completar o cadastro
@@ -11,14 +11,13 @@ app.controller('formCtrl', function($scope,$window){
 	};
 	$scope.registrar = function(){
 		//realizará o cadastro
+		console.log("cadastrado com sucesso")
 	};
-});
-app.controller('ExampleController', function($scope){	
 	$scope.resposta = {
 		valor: 'sim'
 	};
 	$scope.enviar = function(){
 		//enviará o formulário de respostas
 		console.log("Enviado com sucesso!");
-	};		
+	};	
 });
